7011ba77fd8fd42d05b305789a86e3d3fdbc5df4
Kyle Reilly <kjreills@gmail.com>
2021-06-20 15:59:36 -0400

    Updating wallet.py to talk to Gridcoin WebApi instead of RPC

--- server/wallet.py
+++ server/wallet.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 import simplejson as json
 from decimal import *
 from typing import List
@@ -15,14 +16,44 @@ class DecimalEncoder(json.JSONEncoder):
 
 class GridcoinWallet():
 
-    @staticmethod
-    def __request_json(input_method: str, input_parameters: List):
+    from django.conf import settings
+
+    CONTENT_TYPE = {'content-type': 'application/json'}
+
+    __access_token = None
+
+    def __get_auth_token(self):
+        if self.__access_token is not None:
+            return self.__access_token
+
+        request_body = {
+            "client_id": settings.GRIDCOIN_CLIENT_ID,
+            "client_secret": settings.GRIDCOIN_CLIENT_SECRET,
+            "audience": settings.GRIDCOIN_AUDIENCE,
+            "grant_type":"client_credentials"
+        }
+
+        response = requests.post(settings.GRIDCOIN_AUTH_URL, data=json.dumps(request_body), headers=self.CONTENT_TYPE)
+
+        if response.status_code != 200:
+            raise
+        else:
+            content = response.json()
+
+            self.__access_token = content["access_token"]
+
+            return self.__access_token
+
+
+
+
+    def __request_json(url: str, data: Optional[Dict] = None):
         """
         Request JSON data from the GRC full node, given the target command & relevant input parameters.
         More info: http://docs.python-.org/en/master/
         """
 
-        from django.conf import settings
+
 
         rpc_url="http://"+settings.GRIDCOIN_USER+":"+settings.GRIDCOIN_PASSWORD+"@"+settings.GRIDCOIN_DOMAIN+":"+settings.GRIDCOIN_PORT
         headers = {'content-type': 'application/json'}
@@ -38,6 +69,7 @@ class GridcoinWallet():
 
         try:
             requested_data = requests.get(rpc_url, data=json.dumps(dict(payload), use_decimal=True), headers=headers)
+
 
             if requested_data.status_code != 200:
                 return { 'success': False, 'requested_data.status_code':requested_data.status_code, 'response': requested_data.json() }
